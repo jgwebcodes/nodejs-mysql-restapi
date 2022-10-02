41df75e04d9e50e4de8b2e95e3e4c2c1a8303cf9
jgwebcodes <jgwebcodes@gmail.com>
2022-10-02 17:21:48 -0400

    DB_PORT variable addedd

--- src/db.js
+++ src/db.js
@@ -1,11 +1,12 @@
 import { createPool } from "mysql2/promise";
-import { DB_HOST, DB_NAME, DB_USER, DB_PASSWORD } from "./config.js";
+import { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } from "./config.js";
 
 // FOR PRODUCTION
 export const pool = createPool({
     host: DB_HOST,
     user: DB_USER,
+    port: DB_PORT,
     password: DB_PASSWORD,
     database: DB_NAME,
-    ssl: { rejectUnauthorized: false },
+    // ssl: { rejectUnauthorized: false },
 });
